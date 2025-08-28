cb7fe22e537d32f458479b0e8ba5442bd47bff4b
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-08-28 13:25:42 +0700

    landing

---
+++ app/coba/page.tsx
@@ -0,0 +1,16 @@
+import AboutSection from '@/components/about-section'
+import FeatureSection from '@/components/feature-section'
+import HeroSection from '@/components/hero-coba'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <HeroSection />
+      <AboutSection />
+      <FeatureSection />
+    </div>
+  )
+}
+
+export default page
