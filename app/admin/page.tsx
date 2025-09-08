46ee32a481371f85f06fbd8c28cc38f3b0ca1d12
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-09-08 12:53:20 +0700

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,15 @@
 import { DataTableAdminKonsultasi } from "@/components/data-table-admin-konsultasi";
 import { DataProvider } from "../context/data-context";
+import { UserProvider } from "../context/user-context";
 
 export default function AdminPage() {
 	return (
 		<div className="">
-			<DataProvider>
-				<DataTableAdminKonsultasi data={[]} />;
-			</DataProvider>
+			<UserProvider>
+				<DataProvider>
+					<DataTableAdminKonsultasi data={[]} />
+				</DataProvider>
+			</UserProvider>
 		</div>
 	);
 }
