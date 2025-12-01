0087b3259aaa4468f2f4eba8dfc4081b64f10d3a
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-12-01 18:48:10 +0700

    Langing Page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/login-form";
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full bg-white items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
